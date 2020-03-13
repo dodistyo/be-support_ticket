b98a9a25557c04246b7ba9ae6632b4dfa4cac1b8
Dodi Prasetyo <dodipras27@gmail.com>
2020-03-13 17:21:03 +0700

    Changing ORM to use ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -4,16 +4,16 @@ const rootDir = process.env.NODE_ENV === "development" ?
 
 module.exports = {
   "type": "mariadb",
-  "host": "localhost",
-  "port": 3306,
-  "username": "root",
-  "password": "root",
-  "database": "support_ticket",
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
   "synchronize": true,
   "logging": false,
   "entities": [rootDir + "/entity/**/*.{js,ts}"],
   "migrations": [rootDir + "/migration/*.{js,ts}"],
   "subscribers": [rootDir + "/subscriber/**/*.{js,ts}"],
   "seeds": [rootDir + "/migration/seeds/**/*.{js,ts}"],
-  "factories": [rootDir + "/migration/factories/**/*.{js,ts}"],
+  "factories": [rootDir + "/migration/factories/**/*.{js,ts}"]
 } 
